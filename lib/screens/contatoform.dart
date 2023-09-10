c1cfb0e6f68629079fbb84762cb731bd28570f04
User_XQR <joaoldp135@gmail.com>
2023-09-10 18:51:55 +0000

    fixes

--- lib/screens/contatoform.dart
+++ lib/screens/contatoform.dart
@@ -32,25 +32,17 @@ class FormContatoState extends State<FormContato> {
               child: Column(
                 children: <Widget>[
                   Editor(
-                    _controladorNome, 
-                    "Contato", 
-                    "Informe o nome",
-                    Icons.person),
+                      _controladorNome, "Nome", "Informe o nome", Icons.person),
+                  Editor(_controladorEndereco, "Endereço", "Informe o endereço",
+                      Icons.place),
+                  Editor(_controladorEmail, "Email", "Informe o email",
+                      Icons.email),
                   Editor(
-                    _controladorEndereco, 
-                    "Endereço", 
-                    "Informe o endereço",
-                    Icons.place),
-                  Editor(
-                    _controladorEmail, 
-                    "Email", 
-                    "Informe o email",
-                    Icons.email),
-                  Editor(
-                    _controladorFone, 
-                    "Telefone", 
+                    _controladorFone,
+                    "Telefone",
                     "Informe o telefone",
-                    Icons.phone,),
+                    Icons.phone,
+                  ),
                   ElevatedButton(
                       onPressed: () {
                         criarContato(context);
@@ -63,34 +55,43 @@ class FormContatoState extends State<FormContato> {
 
   void criarContato(BuildContext context) {
     ContatoDao dao = ContatoDao();
-    if(_controladorNome.text == ''){
+    if (_controladorNome.text == '') {
       SnackBar snackBar = SnackBar(content: Text("Informe o Nome!"));
       ScaffoldMessenger.of(context).showSnackBar(snackBar);
       return;
-    } else if(_controladorEndereco.text == ''){
+    } else if (_controladorEndereco.text == '') {
       SnackBar snackBar = SnackBar(content: Text("Informe o Endereço!"));
       ScaffoldMessenger.of(context).showSnackBar(snackBar);
       return;
-    }if(_controladorEmail.text == ''){
+    }
+    if (_controladorEmail.text == '') {
       SnackBar snackBar = SnackBar(content: Text("Informe o Email!"));
       ScaffoldMessenger.of(context).showSnackBar(snackBar);
       return;
-    } if(_controladorFone.text == ''){
+    }
+    if (_controladorFone.text == '') {
       SnackBar snackBar = SnackBar(content: Text("Informe o Telefone!"));
       ScaffoldMessenger.of(context).showSnackBar(snackBar);
       return;
-    } 
-    if (_id != null){ // alteração
-      final contato = Contato(_id!,
-          _controladorNome.text, _controladorEndereco.text,
-          _controladorFone.text, _controladorEmail.text);
+    }
+    if (_id != null) {
+      // alteração
+      final contato = Contato(
+          _id!,
+          _controladorNome.text,
+          _controladorEndereco.text,
+          _controladorFone.text,
+          _controladorEmail.text);
       dao.update(contato).then((id) => Navigator.pop(context));
       SnackBar snackBar = SnackBar(content: Text("Contato atualizado!"));
       ScaffoldMessenger.of(context).showSnackBar(snackBar);
     } else {
-      final contato = Contato(_id!,
-          _controladorNome.text, _controladorEndereco.text,
-          _controladorFone.text, _controladorEmail.text);
+      final contato = Contato(
+          _id!,
+          _controladorNome.text,
+          _controladorEndereco.text,
+          _controladorFone.text,
+          _controladorEmail.text);
       dao.save(contato).then((id) {
         print("contato incluído: " + id.toString());
         Navigator.pop(context);
@@ -103,7 +104,7 @@ class FormContatoState extends State<FormContato> {
   @override
   void initState() {
     super.initState();
-    if (widget.contato != null){
+    if (widget.contato != null) {
       _id = widget.contato!.id;
       _controladorNome.text = widget.contato!.nome;
       _controladorEndereco.text = widget.contato!.endereco;
