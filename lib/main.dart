fd1d73a7f130166b75c0a81b69b3f1311e9e6a50
João Luiz Daré Pinto <66640581+nogreenJ@users.noreply.github.com>
2023-09-10 15:31:13 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,16 @@ import 'package:flutter/material.dart';
 import 'menu.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(
+    MaterialApp(
       home: MenuOptions(),
-      theme: ThemeData.dark()
-  ));
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.teal,
+          brightness: Brightness.dark,
+        ),
+      )
+    )
+  );
 }
-
-
-
-
